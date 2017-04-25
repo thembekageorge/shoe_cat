22a54313e70ef7351d1fee1e66288efa295cfecc
thembekageorge <thembiegeorge@gmail.com>
2017-04-25 09:49:58 +0200

    first commit of shoe cat

---
+++ shoe_cat.js
@@ -0,0 +1,6 @@
+document.getElementById('addBootStock').addEventListener("click", practise);
+document.getElementById('showAvailableBoots').addEventListener("click", practise);
+
+function practise(){
+  alert("I work");
+}
