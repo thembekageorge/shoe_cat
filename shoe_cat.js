3fa759c5df7fbf88a771b649cb79a7231b3da002
thembekageorge <thembiegeorge@gmail.com>
2017-04-26 17:52:21 +0200

    added functions on js for the add button on the form

--- shoe_cat.js
+++ shoe_cat.js
@@ -1,13 +1,11 @@
-document.getElementById('addBootStock').addEventListener("click", hideshow);
-// document.getElementById('showAvailableBoots').addEventListener("click", practise);
-
-// function practise(){
-//   alert("I work");
-// }
+document.getElementById('addBootStock').addEventListener("click", hideShowForm);
+document.getElementById('addBoot').addEventListener("click",addBoot);
 
 // The add Stock Part of the project
- function hideshow(){
+ function hideShowForm(){
+
    var getDiv = document.getElementById("adding");
+
     if (getDiv.style.display === 'none')
     {
     getDiv.style.display = 'block';
@@ -15,4 +13,11 @@ document.getElementById('addBootStock').addEventListener("click", hideshow);
     else {
     getDiv.style.display = 'none';
     }
+ }
+
+ function addBoot() {
+   var bootName = document.getElementById('bootname').value;
+   var bootColor = document.getElementById('bootcolor').value;
+   var bootSize = document.getElementById('bootcolor').value;
+alert( bootname);
  }
