696fbcea30aea633bcf96b06b352f67872931a9d
thembekageorge <thembiegeorge@gmail.com>
2017-04-26 15:49:31 +0200

    Added form, coded for form to appear when Add Stock button is pressed

--- shoe_cat.js
+++ shoe_cat.js
@@ -1,6 +1,18 @@
-document.getElementById('addBootStock').addEventListener("click", practise);
-document.getElementById('showAvailableBoots').addEventListener("click", practise);
+document.getElementById('addBootStock').addEventListener("click", hideshow);
+// document.getElementById('showAvailableBoots').addEventListener("click", practise);
 
-function practise(){
-  alert("I work");
-}
+// function practise(){
+//   alert("I work");
+// }
+
+// The add Stock Part of the project
+ function hideshow(){
+   var getDiv = document.getElementById("adding");
+    if (getDiv.style.display === 'none')
+    {
+    getDiv.style.display = 'block';
+    }
+    else {
+    getDiv.style.display = 'none';
+    }
+ }
