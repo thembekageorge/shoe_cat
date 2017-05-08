0e6a0f77d451e0d68073f4f494fef1bdd993eef0
thembekageorge <thembiegeorge@gmail.com>
2017-05-08 10:42:16 +0200

    changed the file names, started over

---
+++ extra_functions.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName('addBootStock').addEventListener('click', hideShowForm);
+ function hideShowForm(){
+   var getDiv = document.getElementsByClassName('add');
+
+    if (getDiv.style.display === 'none')
+    {
+    getDiv.style.display = 'block';
+    }
+    else {
+    getDiv.style.display = 'none';
+    }
+ }
--- shoe_cat.js
+++ shoe_cat.js
@@ -1,22 +1,99 @@
-document.getElementById('addBootStock').addEventListener("click", hideShowForm);
-document.getElementById('addBoot').addEventListener("click",addBoot);
 
-// The add Stock Part of the project
- function hideShowForm(){
+(function() {
+    'use strict';
 
-   var getDiv = document.getElementById("adding");
-
-    if (getDiv.style.display === 'none')
-    {
-    getDiv.style.display = 'block';
+    function initTemplate(templateSelector) {
+        var templateText = document.querySelector(templateSelector).innerHTML;
+        return Handlebars.compile(templateText);
     }
-    else {
-    getDiv.style.display = 'none';
-    }
- }
 
- function addBoot() {
-   var bootName = document.getElementById('bootname').value;
-   var bootColor = document.getElementById('bootcolor').value;
-   var bootSize = document.getElementById('bootcolor').value;
- }
+    var filterTemplate = initTemplate('.filterTemplate');
+    var filters = document.querySelector('.filters');
+
+    var bootsTemplate = initTemplate('.bootsTemplate');
+    var bootsElement = document.querySelector('.bootsList');
+
+    var addButton = document.querySelector('.addButton');
+    var filterButton = document.querySelector('.filterButton');
+
+  var bootName = document.querySelector('.bootName');
+  var bootColor = document.querySelector('.bootColor');
+  var bootSize = document.querySelector('.bootSize');
+  var bootPrice = document.querySelector('.bootPrice');
+  var quantity = document.querySelector('.quantity');
+
+    var boots = [{
+    bootName : 'boot1';
+    bootColor : 'pink'
+    bootSize : 4
+    bootPrice : 400
+    quantity : 3;
+
+    }];
+
+
+        var bootNames = [];
+        var bootColors = [];
+        var bootSizes = [];
+        var bootPrices = [];
+        var quantities = [];
+
+        for (var i = 0; i < data.length; i++) {
+            var bootsData = data[i];
+            bootNames.push(boosData.bootName);
+            bootColors.push(bootsData.bootColor);
+            bootSizes.push(bootsData.bootSize);
+            bootPrices.push(bootsData.bootPrice);
+            quantities.push(bootsData.quantity);
+        }
+
+        filters.innerHTML = filterTemplate({
+            boots: bootNames.sort(),
+            : bootColors.sort(),
+            : bootSizes.sort(function(a,b){
+                return a - b;
+            })
+        });
+
+        bootsElement.innerHTML = bootsTemplate({
+            boots: data
+        });
+    }
+
+    filters.addEventListener('click', function(evt){
+        if (evt.target.name === 'filterButton'){
+            alert('filter!');
+
+            var bootNameFilter = document.querySelector('.bootNameFilter');
+            var bootColorFilter = document.querySelector('.bootColorFilter');
+            var bootSizeFilter = document.querySelector('.bootSizeFilter');
+            var bootPriceFilter = document.querySelector('.bootPriceFilter');
+        }
+    });
+
+    addButton.addEventListener('click', function() {
+        var bootNameValue = bootName.value;
+        var bootColorValue = bootColor.value;
+        var bootSizeValue = bootSize.value;
+        var bootPriceValue = bootPrice.value;
+        var quantity = quantity.value;
+
+
+        if (bootNameValue !== ''
+            && bootColorValue !== ''
+            && bootSizeValue !== '' && bootPriceValue !== '' && quantity !== '') {
+            boots.push({
+              bootName : bootNameValue;
+              bootColor : bootColorValue;
+              bootSize : bootSizeValue;
+              bootPrice : bootPriceValue;
+              quantity : quantity;
+            });
+        }
+
+        showData(boots);
+    });
+
+    showData(boots);
+
+})();
